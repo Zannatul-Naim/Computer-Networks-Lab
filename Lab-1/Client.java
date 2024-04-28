0d939cbfa8af17c4931efb6eb36097160b0c624a
Zannatul Naim <zannatulnaim09@gmail.com>
2024-04-28 14:48:12 +0600

    modified clinet

--- Lab-1/Client.java
+++ Lab-1/Client.java
@@ -1,4 +1,5 @@
 import java.io.DataOutputStream;
+import java.io.DataInputStream;
 import java.net.*;
 import java.util.Scanner;
 
@@ -7,13 +8,20 @@ public class Client {
 
         Socket s = new Socket("localhost", 7878);
         DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+        DataInputStream din = new DataInputStream(s.getInputStream());
+
         Scanner scan = new Scanner(System.in);
         String msg;
+
         do {
-            String str = "Enter a message: ";
-            System.out.println(str);
+            String str = "Client: ";
+            System.out.print(str);
             msg = scan.next();
             dout.writeUTF(msg);
+
+            String server_response_string = (String) din.readUTF();
+            System.out.println("Server: " + server_response_string);
+
         } while(!msg.toLowerCase().equals("quit"));
         
         scan.close();
